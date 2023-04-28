c5e1417d939ec4fb258989f7c4349fc068f52679
RAKESH192121121 <113965912+RAKESH192121121@users.noreply.github.com>
2023-04-28 22:23:19 +0530

    Update 3.playfair.py

--- 3.playfair.py
+++ 3.playfair.py
@@ -1,33 +1,17 @@
-def create_matrix(key):
-    key = key.upper()
-    matrix = [[0 for i in range (5)] for j in range(5)]
-    letters_added = []
-    row = 0
-    col = 0
-    # add the key to the matrix
-    for letter in key:
-        if letter not in letters_added:
-            matrix[row][col] = letter
-            letters_added.append(letter)
+def decrypt():
+    #enter your encrypted message(string) below
+    encrypted_message=input("enter the message to be decrypted:").strip()
+    letters='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    #enter the key value
+    k=int(input("enter the key to decrypt;"))
+    decrypted_message=""
+    for ch in encrypted_message:
+        if ch in letters:
+            position=letters.find(ch)
+            new=(position-k)%26
+            new_char=letters[new]
+            decrypted_message+=new_char
         else:
-            continue
-        if (col==4):
-            col = 0
-            row += 1
-        else:
-            col += 1
-    #Add the rest of the alphabet to the matrix
-    # A=65 ... Z=90
-    for letter in range(65,91):
-        if letter==74: # I/J are in the same position
-                continue
-        if chr(letter) not in letters_added: # Do not add repeated letters
-            letters_added.append(chr(letter))
-            
-    #print (len(letters_added), letters_added)
-    index = 0
-    for i in range(5):
-        for j in range(5):
-            matrix[i][j] = letters_added[index]
-            index+=1
-    return matrix
+            decrypted_message+=ch
+    print(decrypted_message)
+decrypt()
